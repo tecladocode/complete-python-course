50452fccc0984745bb338468802323678c66af38
Jose Salvatierra <jslvtr@gmail.com>
2018-01-11 09:21:50 +0000

    Added first milestone project.

---
+++ section3/milestone_1/app.py
@@ -0,0 +1,53 @@
+movies = []
+
+
+def menu():
+    user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
+    while user_input != 'q':
+        if user_input == 'a':
+            add_movie()
+        elif user_input == 'l':
+            show_movies()
+        elif user_input == 'f':
+            find_by = input("What property of the movie is that? ")
+            looking_for = input("What are you looking for? ")
+            movie = find_movie(looking_for, lambda x: x[find_by])
+            print(movie or 'No movies found.')
+        else:
+            print('Unknown command-please try again.')
+            continue
+        user_input = input("\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
+
+
+def add_movie():
+    name = input("Enter the movie name: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
+
+    movies.append({
+        'name': name,
+        'director': director,
+        'year': year
+    })
+
+
+def show_movies():
+    for movie in movies:
+        show_movie_details(movie)
+
+
+def show_movie_details(movie):
+    print(f"Name: {movie['name']}")
+    print(f"Director: {movie['director']}")
+    print(f"Release year: {movie['year']}")
+
+
+def find_movie(expected, finder):
+    found = []
+    for movie in movies:
+        if finder(movie) == expected:
+            found.append(movie)
+    return found
+
+
+menu()
