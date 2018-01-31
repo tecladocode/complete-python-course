01c87ae985bc4a70fd32c44a945cc23c11244a85
Jose Salvatierra <jslvtr@gmail.com>
2018-01-31 22:47:20 +0000

    Add milestone 2 project—book reading list.

---
+++ section7/milestone_2/utils/database.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple
+
+from utils.database_connection import DatabaseConnection
+
+Book = Tuple(int, str, str, int)
+
+
+def create_book_table() -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        # SQLite automatically makes `integer primary key` row auto-incrementing (see link in further reading)
+        cursor.execute('CREATE TABLE books (id integer primary key, name text, author text, read integer default 0)')
+
+
+def get_all_books() -> List[Book]:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('SELECT * FROM books')
+        books = cursor.fetchall()
+
+    return books
+
+
+def insert_book(name: str, author: str) -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('INSERT INTO books (name, author) VALUES (?, ?)', (name, author))
+
+
+def mark_book_as_read(name: str) -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
+
+
+def delete_book(name: str) -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('DELETE FROM books WHERE name=?', (name,))
---
+++ section7/milestone_2/utils/database_connection.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+class DatabaseConnection:
+    def __init__(self, host: str):
+        self.connection = None
+        self.host = host
+
+    def __enter__(self) -> sqlite3.Connection:
+        self.connection = sqlite3.connect(self.host)
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.connection.close()
