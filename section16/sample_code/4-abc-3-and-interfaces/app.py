958a04658918379d40efd9b96475697dc8c02c9e
Jose Salvatierra <jslvtr@gmail.com>
2018-03-06 09:21:42 +0000

    Add another example of interface usage in app.py

--- section16/sample_code/4-abc-3-and-interfaces/app.py
+++ section16/sample_code/4-abc-3-and-interfaces/app.py
@@ -1,5 +1,7 @@
 from database import Database
 from admin import Admin
+from user import User
+from saveable import Saveable
 
 a = Admin('paco', 'perez', 2)
 b = Admin('rolf', 'smith', 1)
@@ -10,3 +12,11 @@ b.save()
 user = Database.find(lambda x: x['username'] == 'paco')[0]
 user_obj = Admin(**user)
 print(user_obj.username)
+
+print(isinstance(user_obj, Saveable))  # This is True because it's a subclass
+
+# You can do things like these without worry:
+
+users_to_save = [a, b, User('jose', '1234')]
+for u in users_to_save:
+    u.save()  # This is fine, because all users (Admin and User) implement the Saveable interface so we know they have a .save() method
