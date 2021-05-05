6c4fd48302042fabf8c1a300f73efc35a8d80c9f
Vlad Dragusanu <vlad.dragusanu@gmail.com>
2021-05-05 05:49:15 +0200

    Fix scrollbar and CTRL+Quit bug for issue #14

--- course_contents/19_gui_development_tkinter/lectures/17_adding_permanent_scrollbar/app.py
+++ course_contents/19_gui_development_tkinter/lectures/17_adding_permanent_scrollbar/app.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 
@@ -7,19 +8,17 @@ text_contents = dict()
 def check_for_changes():
     current = get_text_widget()
     content = current.get("1.0", "end-1c")
-    name = notebook.tab("current")["text"]
+    name = notebook.tab('current')['text']
 
     if hash(content) != text_contents[str(current)]:
-        if name[-1] != "*":
-            notebook.tab("current", text=name + "*")
-    elif name[-1] == "*":
-        notebook.tab("current", text=name[:-1])
+        if name[-1] != '*':
+            notebook.tab('current', text=name + '*')
+    elif name[-1] == '*':
+        notebook.tab('current', text=name[:-1])
 
 
 def get_text_widget():
-    current_tab = notebook.nametowidget(notebook.select())
-    text_widget = current_tab.winfo_children()[0]
-
+    text_widget = notebook.nametowidget(notebook.select())
     return text_widget
 
 
@@ -35,16 +34,15 @@ def close_current_tab():
 
 
 def current_tab_unsaved():
-    text_widget = get_text_widget()
-    content = text_widget.get("1.0", "end-1c")
-    return hash(content) != text_contents[str(text_widget)]
+    current_tab_name = notebook.tab('current')['text']
+    return current_tab_name[-1] == "*"
 
 
 def confirm_close():
     return messagebox.askyesno(
-        message="You have unsaved changes. Are you sure you want to close?",
-        icon="question",
-        title="Unsaved changes",
+        message='You have unsaved changes. Are you sure you want to close?',
+        icon='question',
+        title='Unsaved Changes'
     )
 
 
@@ -54,47 +52,53 @@ def confirm_quit():
     for tab in notebook.tabs():
         tab_widget = root.nametowidget(tab)
         text_widget = tab_widget.winfo_children()[0]
-        content = text_widget.get("1.0", "end-1c")
+        content = text_widget.get('1.0', 'end-1c')
 
         if hash(content) != text_contents[str(text_widget)]:
             unsaved = True
             break
 
     if unsaved and not confirm_close():
-        return
+        confirm = messagebox.askyesno(
+            message='You have unsaved changes. Are you sure you want to quit?',
+            icon='question',
+            title='Confirm Quit'
+        )
 
-    root.destroy()
+        if not confirm:
+            return
+
+        root.destroy()
 
 
-def create_file(content="", title="Untitled"):
-    container = ttk.Frame(notebook)
-    container.pack()
+def create_file(content='', title='Untitled'):
+    text_area = tk.Text(notebook)
+    text_area.insert('end', content)
+    text_area.pack(side='left', fill="both", expand=True)
 
-    text_area = tk.Text(container)
-    text_area.insert("end", content)
-    text_area.pack(side="left", fill="both", expand=True)
-
-    notebook.add(container, text=title)
-    notebook.select(container)
+    notebook.add(text_area, text=title)
+    notebook.select(text_area)
 
     text_contents[str(text_area)] = hash(content)
 
-    text_scroll = ttk.Scrollbar(container, orient="vertical", command=text_area.yview)
+    text_scroll = tk.Scrollbar(text_area, orient='vertical', command=text_area.yview)
+
     text_scroll.pack(side="right", fill="y")
-    text_area["yscrollcommand"] = text_scroll.set
+
+    text_area['yscrollcommand'] = text_scroll.set
 
 
 def open_file():
     file_path = filedialog.askopenfilename()
 
     try:
-        filename = file_path.split("/")[-1]
+        filename = os.path.basename(file_path)
 
-        with open(file_path, "r") as file:
+        with open(file_path, 'r') as file:
             content = file.read()
 
     except (AttributeError, FileNotFoundError):
-        print("Open operation cancelled")
+        print('Open operation cancelled')
         return
 
     create_file(content, filename)
@@ -104,34 +108,35 @@ def save_file():
     file_path = filedialog.asksaveasfilename()
 
     try:
-        filename = file_path.split("/")[-1]
-        text_widget = get_text_widget()
-        content = text_widget.get("1.0", "end-1c")
+        filename = os.path.basename(file_path)
+        text_wgt = get_text_widget()
+        content = text_wgt.get('1.0', 'end-1c')
 
-        with open(file_path, "w") as file:
+        with open(file_path, 'w') as file:
             file.write(content)
 
     except (AttributeError, FileNotFoundError):
-        print("Save operation cancelled")
+        print('Save operation cancelled')
         return
 
-    notebook.tab("current", text=filename)
-    text_contents[str(text_widget)] = hash(content)
+    notebook.tab('current', text=filename)
+    text_contents[str(text_wgt)] = hash(content)
 
 
 def show_about_info():
     messagebox.showinfo(
-        title="About",
-        message="The Teclado Text Editor is a simple tabbed text editor designed to help you learn Tkinter!",
+        title='About',
+        message='The Teclado Text Editor is a simple tabbed text editor designed to help you learn Tkinter!'
     )
 
 
 root = tk.Tk()
-root.title("Teclado Text Editor")
-root.option_add("*tearOff", False)
+root.geometry('600x400')
+root.title('Teclado Text Editor')
+root.option_add('*tearOff', False)
 
 main = ttk.Frame(root)
-main.pack(fill="both", expand=True, padx=(1), pady=(4, 0))
+main.pack(fill="both", expand=True, padx=1, pady=(4, 0))
 
 menubar = tk.Menu(root)
 root.config(menu=menubar)
@@ -139,18 +144,16 @@ root.config(menu=menubar)
 file_menu = tk.Menu(menubar)
 help_menu = tk.Menu(menubar)
 
-menubar.add_cascade(menu=file_menu, label="File")
-menubar.add_cascade(menu=help_menu, label="Help")
+menubar.add_cascade(menu=file_menu, label='File')
+menubar.add_cascade(menu=help_menu, label='Help')
 
-file_menu.add_command(label="New", command=create_file, accelerator="Ctrl+N")
-file_menu.add_command(label="Open...", command=open_file, accelerator="Ctrl+O")
-file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
-file_menu.add_command(
-    label="Close Tab", command=close_current_tab, accelerator="Ctrl+Q"
-)
-file_menu.add_command(label="Exit", command=confirm_quit)
+file_menu.add_command(label='New', command=create_file, accelerator='Ctrl+N')
+file_menu.add_command(label='Open', command=open_file, accelerator='Ctrl+O')
+file_menu.add_command(label='Save', command=save_file, accelerator='Ctrl+S')
+file_menu.add_command(label='Close Tab', command=close_current_tab, accelerator='Ctrl+Q')
+file_menu.add_command(label='Exit', command=confirm_quit)
 
-help_menu.add_command(label="About", command=show_about_info)
+help_menu.add_command(label='Help', command=show_about_info)
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
