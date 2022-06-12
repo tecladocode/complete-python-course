111314c2271ab089309708029a333c45e748e544
shadowyogi <m.iron@icloud.com>
2022-06-12 16:34:15 +0200

    Revert "clean up"
    This reverts commit beac9053ae74409d6c18c40dffaad41f89ed51cf.

--- my_exercises/stack_class.py
+++
@@ -1,9 +0,0 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def push(self, e):
-        self.items = [e] + self.items
-
-    def pop(self):
-        return self.items.pop(0)
